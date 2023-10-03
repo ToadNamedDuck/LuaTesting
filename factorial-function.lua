fb8fc43ae6a12e8becae6b864ccfb0dcc01244ba
Jason Harris <lord.jtharris@gmail.com>
2023-10-02 19:43:06 -0500

    Wrote a function that calculates a number's factorial in Lua

---
+++ factorial-function.lua
@@ -0,0 +1,18 @@
+factorial = function(num)
+    local total = 1
+    do
+        if (num < 0)
+        then return nil
+        else
+            do
+                for i=1, num
+                    do total = total * i
+                    end
+                end
+                return total
+            end
+        end
+
+    end
+
+print(factorial(10))
